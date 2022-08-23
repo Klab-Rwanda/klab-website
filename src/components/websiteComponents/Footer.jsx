7469f0fc91609f10105c123b04af320cea5f3d95
issansabimana <issa.nsabimana@codeland.it>
2022-08-23 09:53:39 +0200

    Footer

--- src/components/websiteComponents/Footer.jsx
+++ src/components/websiteComponents/Footer.jsx
@@ -1,15 +1,98 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../../public/assets/img/website/images/logo.svg';
 
 const Footer = () => {
   return (
     <div className='footer-cmp'>
-      <p>
-        Made with full focus and 💚 by
-        <b>
-          <a href='https://github.com/mansurissa'> Issa</a>
-        </b>
-      </p>
-      <p>&copy; 2022 Issa NSABIMANA</p>
+      <div className='container'>
+        <div className='upper-footer'>
+          <div className='grid'>
+            <h2>Klab</h2>
+            <div className='footer-links'>
+              <div className='footer-link'>
+                <Link to='/'>About us</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Our team</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Blog</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Terms and servises</Link>
+              </div>
+            </div>
+          </div>
+          <div className='grid'>
+            <h2>Klab</h2>
+            <div className='footer-links'>
+              <div className='footer-link'>
+                <Link to='/'>About us</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Our team</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Blog</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Terms and servises</Link>
+              </div>
+            </div>
+          </div>
+          <div className='grid'>
+            <h2>Klab</h2>
+            <div className='footer-links'>
+              <div className='footer-link'>
+                <Link to='/'>About us</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Our team</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Blog</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Terms and servises</Link>
+              </div>
+            </div>
+          </div>
+          <div className='grid'>
+            <h2>Klab</h2>
+            <div className='footer-links'>
+              <div className='footer-link'>
+                <Link to='/'>About us</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Our team</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Blog</Link>
+              </div>
+              <div className='footer-link'>
+                <Link to='/'>Terms and servises</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='lower-footer'>
+          <div className='logo'>
+            <Link to={'/'}>
+              <a>
+                <img src={Logo} alt='logo' />
+              </a>
+            </Link>
+          </div>
+          <div className='social'>social medias</div>
+          <p>
+            &copy; 2022 Klab Rwanda. Made with passion by
+            <b>
+              <a href='https://github.com/mansurissa'> Issa 💚</a>
+            </b>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ import NavBar from '../components/websiteComponents/Navbar';
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className='landingPage'>
       <NavBar />
       <div>Home page</div>
       <Footer />
