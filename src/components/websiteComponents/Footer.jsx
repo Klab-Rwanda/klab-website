2ebabe683a0664461b8acd02b25685d54ed80710
issansabimana <issa.nsabimana@codeland.it>
2022-08-29 22:02:03 +0200

    Footer finalization

--- src/components/websiteComponents/Footer.jsx
+++ src/components/websiteComponents/Footer.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../../public/assets/img/website/images/logo.svg';
+import Logo from '/assets/img/website/images/logo.svg';
+import FB from '/assets/img/website/icons/fb.svg';
+import TWITTER from '/assets/img/website/icons/twitter.svg';
+import LINKEDIN from '/assets/img/website/icons/linkedin.svg';
+import YOUTUBE from '/assets/img/website/icons/youtube.svg';
+import IG from '/assets/img/website/icons/ig.svg';
+import CALL from '/assets/img/website/icons/call.svg';
+import LOCATION from '/assets/img/website/icons/location.svg';
+import EMAIL from '/assets/img/website/icons/email.svg';
 
 const Footer = () => {
   return (
@@ -25,54 +33,51 @@ const Footer = () => {
             </div>
           </div>
           <div className='grid'>
-            <h2>Klab</h2>
+            <h2>Need help?</h2>
             <div className='footer-links'>
-              <div className='footer-link'>
-                <Link to='/'>About us</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Our team</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Blog</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Terms and servises</Link>
+              <div className='footer-link flex'>
+                <img src={LOCATION} alt='location' />
+                <p>Kigali, Gasabo KG 2324 st</p>
+              </div>{' '}
+              <div className='footer-link flex'>
+                <img src={CALL} alt='call' />
+                <p>+25078999943</p>
+              </div>{' '}
+              <div className='footer-link flex'>
+                <img src={EMAIL} alt='email' />
+                <p>info@klab.com</p>
               </div>
             </div>
           </div>
           <div className='grid'>
-            <h2>Klab</h2>
+            <h2>Programs</h2>
             <div className='footer-links'>
               <div className='footer-link'>
-                <Link to='/'>About us</Link>
+                <Link to='/'>Klab startpus academy</Link>
               </div>
               <div className='footer-link'>
-                <Link to='/'>Our team</Link>
+                <Link to='/'>Future coder</Link>
               </div>
               <div className='footer-link'>
-                <Link to='/'>Blog</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Terms and servises</Link>
+                <Link to='/'>Another program</Link>
               </div>
             </div>
           </div>
           <div className='grid'>
-            <h2>Klab</h2>
+            <h2>Our newsletter</h2>
             <div className='footer-links'>
-              <div className='footer-link'>
-                <Link to='/'>About us</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Our team</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Blog</Link>
-              </div>
-              <div className='footer-link'>
-                <Link to='/'>Terms and servises</Link>
-              </div>
+              <p className='cta'>
+                Get latest news and updates from us and all over the worl by
+                subscribing to our news letter.
+              </p>
+              <form>
+                <div>
+                  <input type='text' placeholder='Enter your email' />
+                </div>
+                <button type='submit' className='button'>
+                  Subscribe
+                </button>
+              </form>
             </div>
           </div>
         </div>
@@ -84,7 +89,33 @@ const Footer = () => {
               </a>
             </Link>
           </div>
-          <div className='social'>social medias</div>
+          <div className='socials'>
+            <div className='item'>
+              <Link to='/'>
+                <img src={FB} alt='fb' />
+              </Link>
+            </div>
+            <div className='item'>
+              <Link to='/'>
+                <img src={TWITTER} alt='twitter' />
+              </Link>
+            </div>
+            <div className='item'>
+              <Link to='/'>
+                <img src={YOUTUBE} alt='twitter' />
+              </Link>
+            </div>
+            <div className='item'>
+              <Link to='/'>
+                <img src={IG} alt='twitter' />
+              </Link>
+            </div>
+            <div className='item'>
+              <Link to='/'>
+                <img src={LINKEDIN} alt='twitter' />
+              </Link>
+            </div>
+          </div>
           <p>
             &copy; 2022 Klab Rwanda. Made with passion by
             <b>
