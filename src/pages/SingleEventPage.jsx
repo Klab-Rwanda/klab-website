fad99a36881221fcb6d6daf8718744aea919f525
issansabimana <issa.nsabimana@codeland.it>
2022-09-16 15:50:31 +0200

    single event

--- src/pages/SingleEventPage.jsx
+++ src/pages/SingleEventPage.jsx
@@ -5,6 +5,7 @@ import HERO_BANNER_IMG from '/assets/website/images/event.png';
 import BlueDecoration from '/assets/website/decorations/blue.svg';
 import WhiteDecoration from '/assets/website/decorations/white.svg';
 import EventCard from '../components/websiteComponents/EventCard';
+import HeadIntro from '../components/websiteComponents/HeadIntro';
 
 const SingleEventPage = () => {
   return (
@@ -40,6 +41,66 @@ const SingleEventPage = () => {
 
           <div className='colored-decorations '>
             <img src={BlueDecoration} alt='decoration' />
+          </div>
+        </div>
+        <div className='container-default'>
+          <div className='single-event-body '>
+            <div className='desc-part'>
+              <div className='title'>
+                <HeadIntro title='Event description' />
+              </div>
+              <div className='body'>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of
+                  Letraset sheets containing Lorem Ipsum passages,
+                </p>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of
+                  Letraset sheets containing Lorem Ipsum passages,
+                </p>
+              </div>
+              <form className='form'>
+                <p className='title-xl'>Leave Comment</p>
+                <div className='input'>
+                  <input type='text' name='name' placeholder='Full name' />
+                </div>
+                <div className='input'>
+                  <input type='email' name='email' placeholder='Email' />
+                </div>
+                <div className='text-area'>
+                  <textarea name='name' placeholder='Full name' />
+                </div>
+                <button className='button' type='submit'>
+                  Send
+                </button>
+              </form>
+            </div>
+            <div className='other-info-part'>
+              <div className='title-xl'>Event Location</div>
+              <div className='map'>
+                <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.5327136866454!2d30.08853461543378!3d-1.939467698586016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6eb4b136305%3A0xfa7ecaf4c40f3383!2skLab!5e0!3m2!1sen!2srw!4v1662556691905!5m2!1sen!2srw'></iframe>
+              </div>
+              <div className='title-xl'>Tags</div>
+              <div className='tags'></div>
+            </div>
           </div>
         </div>
       </div>
