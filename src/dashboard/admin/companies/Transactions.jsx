53de3a66068b0ffc8a003b076ac1ceeec7c2d7c6
Blandine <blandinemunezero8@gmail.com>
2023-03-10 08:40:37 -0800

    added projects,trainers and transactions

---
+++ src/dashboard/admin/companies/Transactions.jsx
@@ -0,0 +1,144 @@
+import {GrMore} from 'react-icons/gr';
+import profile from '/public/assets/website/images/girl.jpg';
+const Transactions=()=>{
+    return(
+    
+        <div className='company-container'>
+            <div className="company-headers">
+                <h2>Latest Transactions</h2>
+            </div>
+
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+            <div className="company-members">
+                <div className="company-item">
+                    <img src={profile}/>
+               
+                 <div className="company-info-container">
+                    <div className="company-names">
+                        <h4>Heaven Studio</h4>
+                        <p>heavenstudio@gmail.com</p>
+                    </div>
+                    <div className='dollar-sign'>
+                    <p>+$250.00</p>
+                    </div>
+                    <div className="more-icon"><GrMore/></div>
+                 </div>
+                </div>
+            </div>
+
+        </div>
+);
+}
+export default Transactions;
--- src/dashboard/admin/companies/projects.jsx
+++ src/dashboard/admin/companies/projects.jsx
@@ -1,74 +1,15 @@
-// import React from 'react'
-
-import Outline from './outline';
 import {AiTwotoneCalendar} from 'react-icons/ai';
 import Profile from '/src/asset/Profile.png';
-import {FaCalendar, FaUsers, FaBell} from "react-icons/fa";
-import {MdApartment} from "react-icons/md";
 import {FaPlus} from "react-icons/fa";
-import Sidebar from "../secondSidebar";
-
 
 const Projectpage=()=>{
     return(
 
-          <div className="wrapper">
-         <Outline />  
-         <section className="projects-content">
-
-         <div className="event-cards">
-          <div className="card-item">
-            <div className="card-details">
-              <p>All Events</p>
-              <h1>36</h1>
-            </div>
-            <div className="card-icon allevents">
-              <FaCalendar />
-            </div>
-          </div>
-
-          <div className="card-item">
-            <div className="card-details">
-              <p>New Events</p>
-              <h1>11</h1>
-            </div>
-            <div className="card-icon new-events">
-              <MdApartment />
-            </div>
-          </div>
-
-          <div className="card-item">
-            <div className="card-details">
-              <p>Hackathons</p>
-              <h1>08</h1>
-            </div>
-            <div className="card-icon hackathons">
-              <FaUsers />
-            </div>
-          </div>
-
-          <div className="card-item">
-            <div className="card-details">
-              <p>Competitions</p>
-              <h1>08</h1>
-            </div>
-            <div className="card-icon competitons">
-              <FaBell />
-            </div>
-          </div>
-        </div>
 
         <div className='projects-section'>
-
-        
-        <div clasName="projects-sidebar">
-        <Sidebar />
-        </div>
-
-        
-         <div className='card'>
+          <div className='card'>
           <div className='head'>  
-         <div className='card7' head>
+          <div className='card7' head>
             <h1>Open</h1>
             <div className='icon'><FaPlus/></div>
             </div>
@@ -94,23 +35,23 @@ const Projectpage=()=>{
                 <AiTwotoneCalendar/>
                 <p>DueMarch24</p>
             </div>
-        </div>
-        <div className='paragraph'>
+         </div>
+         <div className='paragraph'>
             <p>Conduct to research <br/>landing page flex jobs</p></div>
-        <div class="progress-container">
+         <div class="progress-container">
             <div class="progress">
                 <div class="progress-inside"></div>
             </div>
             <p>78%</p>
-        </div>
-        <div class="profi">
-        <img src={Profile} alt="Profile" />
-        <img src={Profile} alt="Profile" />
-        <img src={Profile} alt="Profile" />
-        </div>
-        </div>
+         </div>
+         <div class="profi">
+         <img src={Profile} alt="Profile" />
+         <img src={Profile} alt="Profile" />
+         <img src={Profile} alt="Profile" />
+         </div>
+         </div>
 
-        <div class="card2 cards">
+         <div class="card2 cards">
          <div class="headera">
             <div className='one'>UI Design</div>
             <div class="calender">
@@ -224,9 +165,9 @@ const Projectpage=()=>{
         </div>
         </div>
         </div>
-        </div>
-    </section>
-</div>
+    </div>
+    
+
 )
     }
 
